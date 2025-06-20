3b37310fc5c9786c84576e951f628fccf0958580
randolf <136251983+randolfspages@users.noreply.github.com>
2025-06-20 10:54:54 +0200

    next-config update

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,32 @@ const nextConfig: NextConfig = {
         hostname: 'lh3.googleusercontent.com',
         protocol: 'https',
         port: '',
-      }
+      },
+      {
+        hostname: 'pixabay.com',
+        protocol: 'https',
+        port: '',
+      },
+      {
+        hostname: 'chuckmissler.com',
+        protocol: 'https',
+        port: '',
+      },
+      {
+        hostname: 'ground.news',
+        protocol: 'https',
+        port: '',
+      },
+      {
+        hostname: 'derekprince.com',
+        protocol: 'https',
+        port: '',
+      },
+      {
+        hostname: 'johnlennox.org',
+        protocol: 'https',
+        port: '',
+      },
     ]
   }
 };
