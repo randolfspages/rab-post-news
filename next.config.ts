0549631068e1e4066a77acaf0cabee56a30aafa5
randolf <136251983+randolfspages@users.noreply.github.com>
2025-06-20 11:12:27 +0200

    configuration to allow  all hostnames

--- next.config.ts
+++ next.config.ts
@@ -5,37 +5,7 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        hostname: 'images.pexels.com',
-        protocol: 'https',
-        port: '',
-      },
-      {
-        hostname: 'lh3.googleusercontent.com',
-        protocol: 'https',
-        port: '',
-      },
-      {
-        hostname: 'pixabay.com',
-        protocol: 'https',
-        port: '',
-      },
-      {
-        hostname: 'chuckmissler.com',
-        protocol: 'https',
-        port: '',
-      },
-      {
-        hostname: 'ground.news',
-        protocol: 'https',
-        port: '',
-      },
-      {
-        hostname: 'derekprince.com',
-        protocol: 'https',
-        port: '',
-      },
-      {
-        hostname: 'johnlennox.org',
+        hostname: '**',
         protocol: 'https',
         port: '',
       },
